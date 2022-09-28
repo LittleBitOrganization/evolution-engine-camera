9294236fed4c7c00fa021dc232d22f7a93385e9b
Terentev Vlad <42178405+TerentevVlad@users.noreply.github.com>
2022-09-28 16:04:15 +0300

    Add CheckBorders method in construct

--- Runtime/Services/CameraService.cs
+++ Runtime/Services/CameraService.cs
@@ -45,6 +45,7 @@ public class CameraService : IDisposable
             
             SubscribeOnTouchInputEvents();
             UpdateCamParameters();
+            CheckBorders();
         }
 
         private void UnSubscribeOnTouchInputEvents()
